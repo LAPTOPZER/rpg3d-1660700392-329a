6442989f361f027dbe42fd42f414b6f577956c92
LAPTOPZER <vakadarbhafill@gmail.com>
2026-01-29 19:50:25 +0700

    Character Prefab

---
+++ Assets/_Custom/Scripts/Animation/CharAnimation.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class CharAnimation : MonoBehaviour
+{
+    private Characters character;
+
+    void Start()
+    {
+        character = GetComponent<Characters>();
+    }
+
+    void Update()
+    {
+        ChooseAnimation(character);
+    }
+
+    private void ChooseAnimation(Characters c)
+    {
+        c.Anim.SetBool("IsIdle", false);
+        c.Anim.SetBool("IsWalk", false);
+
+        switch (c.State)
+        {
+            case CharState.Idle:
+                c.Anim.SetBool("IsIdle", true);
+                break;
+            case CharState.Walk:
+                c.Anim.SetBool("IsIdle", true);
+                break;
+        }
+    }
+}
